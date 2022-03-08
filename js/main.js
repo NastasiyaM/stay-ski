865b0a0bae0231d3a545af19b3248943ad56aec9
Anastasiya Mamontova <79529646321@ya.ru>
2022-03-08 17:44:37 +0300

    contacts css

--- js/main.js
+++ js/main.js
@@ -2,8 +2,28 @@ $(function(){
     $('.testimonials__slider').slick({
     arrows: false,    
     dots: true,
-    fade: true,
-    
+    fade: true,    
 
 });
 });
+
+
+let center = [47.810344, 13.047994];
+
+function init() {
+	let map = new ymaps.Map('map-test', {
+		center: center,
+		zoom: 15
+	});
+
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init);
